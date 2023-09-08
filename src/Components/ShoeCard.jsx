643d5412ba8147904f568147e30dfc424f465df3
joaopbs98 <joaopbs98@gmail.com>
2023-09-08 16:00:59 +0100

    feat: Complete hero section
    This commit marks the completion of the hero section. The hero component is now fully implemented and functional, showcasing our website's main content. This significant milestone brings us one step closer to our project's goals and enhances the user experience.
    
    #progress #hero #completed

---
+++ src/Components/ShoeCard.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+const ShoeCard = ({ imageURL, changeBigShoeImage, bigShoeImage }) => {
+  const handleClick = () => {
+    if (bigShoeImage !== imgURL.bigShoe) {
+      changeBigShoeImage(imgURL.bigShoe);
+    }
+  };
+  return (
+    <div
+      className={`border-2 rounded-xl ${
+        bigShoeImage === imgURL ? "border-coral-red" : "border-transparent"
+      } cursor-pointer max-sm:flex-1 `}
+      onClick={handleClick}
+    >
+      <div>
+        <img src="imgURL.thumbnail" alt="" />
+      </div>
+    </div>
+  );
+};
+
+export default ShoeCard;
